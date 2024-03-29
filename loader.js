4814232cc3a8c55f003ba3860d48feefd9f4edf8
todplaz <balocm@yahoo.fr>
2024-03-30 00:08:28 +0100

    modify loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 'use client'
 
 export default function myImageLoader({ src, width, quality }) {
-  if(src.starsWith('https://images.pexels.com')) return src
-  return `https://my-portfolio.com/${src}?w=${width}&q=${quality || 75}`
+  if(src.startsWith('https://images.pexels.com')) return src
+  return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`
 }
