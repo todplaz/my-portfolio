c599d5cb87f6865d786b8b96f41b4cb760732d9e
todplaz <balocm@yahoo.fr>
2024-04-10 03:59:33 +0200

    remove loader file

--- loader.js
+++
@@ -1,6 +0,0 @@
-'use client'
-
-export default function myImageLoader({ src, width, quality }) {
-  if(src.startsWith('https://images.pexels.com')) return src
-  return `http://localhost:${process.env.PORT || 3000}/${src}?w=${width}&q=${quality || 75}`
-}
