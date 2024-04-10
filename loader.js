b284a9e71c03bff1483d42eb326c6a6c70921d03
todplaz <balocm@yahoo.fr>
2024-04-10 03:52:09 +0200

    change localhost:3000 to localhost with process.env...

--- loader.js
+++ loader.js
@@ -2,5 +2,5 @@
 
 export default function myImageLoader({ src, width, quality }) {
   if(src.startsWith('https://images.pexels.com')) return src
-  return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`
+  return `http://localhost:${process.env.PORT || 3000}/${src}?w=${width}&q=${quality || 75}`
 }
